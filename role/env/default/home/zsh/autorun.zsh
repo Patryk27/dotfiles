71936c196a4f6d90039677ab0cef74d1fd261e72
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2021-04-26 22:20:50 +0200

    zsh: Make ^W and ^[^H Bash-alike

--- role/env/default/home/zsh/autorun.zsh
+++ role/env/default/home/zsh/autorun.zsh
@@ -1,3 +1,13 @@
+autoload -Uz backward-kill-word-match
+
+bindkey '^W' backward-kill-space-word
+zle -N backward-kill-space-word backward-kill-word-match
+zstyle :zle:backward-kill-space-word word-style space
+
+bindkey '^[^H' backward-kill-bash-word
+zle -N backward-kill-bash-word backward-kill-word-match
+zstyle :zle:backward-kill-bash-word word-style bash
+
 function dip() {
     docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
 }
