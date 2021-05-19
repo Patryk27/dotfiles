326a86e725dedf82eeb98314eaa25e1c35a55ddc
Patryk Wychowaniec <pwychowaniec@pm.me>
2021-05-19 14:44:22 +0200

    zsh: Add madison-attach, madison-new & pg-rust

--- role/env/default/home/zsh/autorun.zsh
+++ role/env/default/home/zsh/autorun.zsh
@@ -8,15 +8,60 @@ bindkey '^[^H' backward-kill-bash-word
 zle -N backward-kill-bash-word backward-kill-word-match
 zstyle :zle:backward-kill-bash-word word-style bash
 
-function dip() {
+function d-ip() {
     docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
 }
 
-function scr-save() {
-    if [[ -f "$1" ]]; then
-        echo "error: file already exists: $1"
+function madison-attach {
+    session="$1"
+
+    if [[ -z "$session" ]]; then
+        echo "usage: madison-attach <session>"
         return
     fi
 
-    mv /tmp/screenshot.png "$1"
+    TERM=xterm-24bit ssh madison -t tmux attach -t "$session"
+}
+
+function madison-new {
+    session="$1"
+
+    if [[ -z "$session" ]]; then
+        echo "usage: madison-new <session>"
+        return
+    fi
+
+    TERM=xterm-24bit ssh madison -t tmux new -s "$session"
+}
+
+function src-save() {
+    file="$1"
+
+    if [[ -z "$file" ]]; then
+        echo "usage: scr-save <file>"
+        return
+    fi
+
+    if [[ -f "$file" ]]; then
+        echo "error: file already exists: $file"
+        return
+    fi
+
+    mv /tmp/screenshot.png "$file"
+}
+
+function pg-rust {
+    dir=$(mktemp -d -t pg-XXXXXXXXXX)
+    cd "$dir"
+
+    cargo init --name playground
+
+    emacs -nw \
+        -f "+vterm/here" \
+        --eval '(vterm-send-string "watch -n1 cargo run")' \
+        --eval "(vterm-send-return)" \
+        -f "split-window-horizontally" \
+        "$dir/src/main.rs" \
+        -f "split-window-vertically" \
+        "$dir/Cargo.toml"
 }
