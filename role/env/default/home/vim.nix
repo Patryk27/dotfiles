a189968fd7b0f5d50c0f192d80f2d49bb4a58523
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2021-09-20 19:21:29 +0200

    vim: Simplify config

--- role/env/default/home/vim.nix
+++ role/env/default/home/vim.nix
@@ -1,52 +1,38 @@
 { pkgs, ... }: {
-  home-manager.users.pwy =
-    let
-      firenvim = {
-        plugin = pkgs.vimUtils.buildVimPlugin {
-          name = "firenvim";
-          src = pkgs.firenvim;
-        };
-      };
+  home-manager.users.pwy = {
+    programs = {
+      neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
 
-    in
-    {
-      programs = {
-        neovim = {
-          enable = true;
-          viAlias = true;
-          vimAlias = true;
+        extraConfig = ''
+          set tabstop=4
+          set shiftwidth=4
+          set expandtab
+          set nobackup
 
-          plugins = [
-            firenvim
-          ];
+          filetype plugin indent on
 
-          extraConfig = ''
-            set tabstop=4
-            set shiftwidth=4
-            set expandtab
-            set nobackup
+          au BufEnter github.com_*.txt set filetype=markdown
+          au BufEnter git.anixe.pl_*.txt set filetype=markdown
 
-            filetype plugin indent on
-
-            au BufEnter github.com_*.txt set filetype=markdown
-            au BufEnter git.anixe.pl_*.txt set filetype=markdown
-
-            let g:firenvim_config = {
-                \ 'globalSettings': {
-                    \ 'alt': 'all',
-                \  },
-                \ 'localSettings': {
-                    \ '.*': {
-                        \ 'cmdline': 'neovim',
-                        \ 'content': 'text',
-                        \ 'priority': 0,
-                        \ 'selector': 'textarea',
-                        \ 'takeover': 'never',
-                    \ },
-                \ }
-            \ }
-          '';
-        };
+          let g:firenvim_config = {
+              \ 'globalSettings': {
+                  \ 'alt': 'all',
+              \  },
+              \ 'localSettings': {
+                  \ '.*': {
+                      \ 'cmdline': 'neovim',
+                      \ 'content': 'text',
+                      \ 'priority': 0,
+                      \ 'selector': 'textarea',
+                      \ 'takeover': 'never',
+                  \ },
+              \ }
+          \ }
+        '';
       };
     };
+  };
 }
