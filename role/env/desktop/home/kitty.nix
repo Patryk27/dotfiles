3ea94549e0cdeaea39b28d170aa5bb21f4d891f3
Patryk Wychowaniec <pwychowaniec@pm.me>
2022-06-04 12:51:44 +0200

    kitty: Disable confirm_os_window_close

--- role/env/desktop/home/kitty.nix
+++ role/env/desktop/home/kitty.nix
@@ -13,6 +13,7 @@
           include ${pkgs.kitty-themes}/themes/rose-pine-dawn.conf
           enable_audio_bell no
           placement_strategy top-left
+          confirm_os_window_close 0
         '';
       };
     };
