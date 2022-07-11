969ca3f3d73c12d76dd5edd8fd7cbc360396aecd
Patryk Wychowaniec <pwychowaniec@pm.me>
2022-07-11 19:25:04 +0200

    emacs: Get rid of custom diagnostics

--- role/env/desktop/home/emacs/doom.d/packages.el
+++ role/env/desktop/home/emacs/doom.d/packages.el
@@ -1,5 +1,3 @@
-(package! flycheck-popup-tip
-  :disable t)
 
 (package! lsp-mode
   :pin "465bcc5fbeb6293446a3241f5f2c1b872337958c")
@@ -15,10 +13,6 @@
 (package! parrot
   :recipe (:host github :repo "dp12/parrot")
   :pin "29265d118267e524453aaa9121c4eae213a63164")
-
-(package! quick-peek
-  :recipe (:host github :repo "cpitclaudel/quick-peek")
-  :pin "03a276086795faad46a142454fc3e28cab058b70")
 
 (package! rustic
   :pin "3b379fc25b7a097a014147d9c8b83ec1a418cd76")
