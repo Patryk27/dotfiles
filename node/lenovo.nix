37555aca4652b072d07fff287d045b68946f16b5
Patryk Wychowaniec <pwychowaniec@pm.me>
2022-10-12 11:10:32 +0200

    lenovo: Split the nvidia specialization

--- node/lenovo.nix
+++ node/lenovo.nix
@@ -250,15 +250,60 @@
   # ---
 
   specialisation = {
-    nvidia = {
+    nvidia-offload = {
       inheritParentConfig = true;
 
       configuration = {
+        environment = {
+          systemPackages = with pkgs; [
+            nvtop
+
+            (writeShellScriptBin "nvidia-offload" ''
+              export __NV_PRIME_RENDER_OFFLOAD=1
+              export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+              export __GLX_VENDOR_LIBRARY_NAME=nvidia
+              export __VK_LAYER_NV_optimus=NVIDIA_only
+              exec "$@"
+            '')
+          ];
+        };
+
         hardware = {
           nvidia = {
             prime = {
-              nvidiaBusId = "PCI:1:0:0";
               intelBusId = "PCI:0:2:0";
+              nvidiaBusId = "PCI:1:0:0";
+
+              offload = {
+                enable = true;
+              };
+            };
+          };
+        };
+
+        services = {
+          xserver = {
+            videoDrivers = [ "nvidia" ];
+          };
+        };
+      };
+    };
+
+    nvidia-sync = {
+      inheritParentConfig = true;
+
+      configuration = {
+        environment = {
+          systemPackages = with pkgs; [
+            nvtop
+          ];
+        };
+
+        hardware = {
+          nvidia = {
+            prime = {
+              intelBusId = "PCI:0:2:0";
+              nvidiaBusId = "PCI:1:0:0";
 
               sync = {
                 enable = true;
