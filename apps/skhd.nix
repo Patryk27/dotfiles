a3d7bf3218690daec1f2c9048e7df04a4aad78b3
Patryk Wychowaniec <pwychowaniec@pm.me>
2023-07-26 14:35:30 +0200

    skhd: Sync

--- apps/skhd.nix
+++ apps/skhd.nix
@@ -1,11 +1,20 @@
-{ ... }: {
+{ pkgs, ... }: {
   services = {
     skhd = {
       enable = true;
 
+      # https://github.com/koekeishiya/skhd/issues/42
+      package = pkgs.writeScriptBin "skhd" ''
+        export SHELL=/bin/bash
+        exec ${pkgs.skhd}/bin/skhd "$@"
+      '';
+
       skhdConfig = ''
         alt - 0x21 : yabai -m space --focus prev
         alt - 0x1E : yabai -m space --focus next
+
+        alt - 0x2B : yabai -m space --focus prev
+        alt - 0x2F : yabai -m space --focus next
       '';
     };
   };
