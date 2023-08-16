8d37a9752784a29372b03f69294a215ad0086aba
Patryk Wychowaniec <pwychowaniec@pm.me>
2023-08-16 09:07:28 +0200

    zsh: Sync

--- apps/zsh/init.zsh
+++ apps/zsh/init.zsh
@@ -8,7 +8,7 @@ bindkey '^[^H' backward-kill-bash-word
 zle -N backward-kill-bash-word backward-kill-word-match
 zstyle :zle:backward-kill-bash-word word-style bash
 
-export PATH="$PATH:/home/pwy/.cargo/bin"
+export PATH="$PATH:$HOME/.cargo/bin"
 
 export GPG_TTY=$(tty)
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
@@ -18,63 +18,49 @@ d-ip() {
     docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
 }
 
-pg-rust() {
-    dir=$(mktemp -d -t pg-XXXXXXXXXX)
-    cd "$dir"
-
-    cargo init --name playground
-
-    emacs \
-        -f "+vterm/here" \
-        --eval '(vterm-send-string "watch -n1 cargo run")' \
-        --eval "(vterm-send-return)" \
-        -f "split-window-horizontally" \
-        "$dir/src/main.rs" \
-        -f "split-window-vertically" \
-        "$dir/Cargo.toml"
-}
-
 pub-add() {
-    fpath="$1"
+    file="$1"
 
-    if [[ -z "$fpath" ]]; then
+    if [[ -z "$file" ]]; then
         echo "usage: pub-add <path>"
         return
     fi
 
-    fname=$(basename "$fpath")
+    fuuid=$(uuidgen | tr '[:upper:]' '[:lower:]')
+    fext=$file:t:e
+    fname="$fuuid.$fext"
 
     rsync \
         -avz \
-        --info=progress \
         --rsync-path="sudo rsync" \
-        "$fpath" \
-        "eric:/var/lib/containers/nginx/var/www/files/$fname"
+        "$file" \
+        "gateway:/var/lib/nixos-containers/nginx/var/www/files/$fname"
 
-    echo "Ok: https://files.pwy.io/$fname"
-}
-
-pub-del() {
-    fname="$1"
-
-    if [[ -z "$fname" ]]; then
-        echo "usage: pub-del <name>"
-        return
-    fi
-
-    ssh eric -- \
-        sudo rm "/var/lib/containers/nginx/var/www/files/$fname"
+    echo
+    echo "https://files.pwy.io/$fname"
 }
 
 pub-ls() {
-    ssh eric -- \
-        ls -l /var/lib/containers/nginx/var/www/files
+    ssh gateway -- \
+        ls -l /var/lib/nixos-containers/nginx/var/www/files
+}
+
+pub-rm() {
+    fname="$1"
+
+    if [[ -z "$fname" ]]; then
+        echo "usage: pub-rm <name>"
+        return
+    fi
+
+    ssh gateway -- \
+        sudo rm "/var/lib/nixos-containers/nginx/var/www/files/$fname"
 }
 
 z() {
     if [[ "$#" == 0 ]]; then
-        fc -ln -1 | xclip
+        fc -ln -1 | pbcopy
     else
-        echo "$@" | xclip
+        echo "$@" | pbcopy
     fi
 }
