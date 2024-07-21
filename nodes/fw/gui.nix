94d514eec212cc4db6fbb522abbcbda1d6519532
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-07-21 10:39:06 +0200

    fw/gui: Setup Gnome keybindings

--- nodes/fw/gui.nix
+++ nodes/fw/gui.nix
@@ -13,6 +13,30 @@
     };
   };
 
+  home-manager.users.pwy = {
+    dconf = {
+      settings = {
+        "org/gnome/desktop/wm/keybindings" = {
+          switch-to-workspace-up = [ "<Super>k" ];
+          switch-to-workspace-down = [ "<Super>j" ];
+          switch-to-workspace-left = [ "<Super>h" ];
+          switch-to-workspace-right = [ "<Super>l" ];
+
+          move-to-workspace-up = [ "<Super><Shift>k" ];
+          move-to-workspace-down = [ "<Super><Shift>j" ];
+          move-to-workspace-left = [ "<Super><Shift>h" ];
+          move-to-workspace-right = [ "<Super><Shift>l" ];
+        };
+      };
+    };
+  };
+
+  programs = {
+    dconf = {
+      enable = true;
+    };
+  };
+
   services = {
     printing = {
       enable = true;
