cf8104fa7cd99529370f57126ccb59751d95cf90
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-11-11 11:33:57 +0100

    gui: Use `services.displayManager.autoLogin`

--- nodes/fw/gui.nix
+++ nodes/fw/gui.nix
@@ -46,6 +46,13 @@
   };
 
   services = {
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = "pwy";
+      };
+    };
+
     printing = {
       enable = true;
 
@@ -67,6 +74,19 @@
         gnome = {
           enable = true;
         };
+      };
+    };
+  };
+
+  # https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd = {
+    services = {
+      "getty@tty1" = {
+        enable = false;
+      };
+
+      "autovt@tty1" = {
+        enable = false;
       };
     };
   };
