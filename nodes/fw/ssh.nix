2e1e844582e80bc9216a33d68a4bb5409f021f02
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-11-28 20:41:45 +0100

    ssh: Refactoring

--- nodes/fw/ssh.nix
+++ nodes/fw/ssh.nix
@@ -2,68 +2,90 @@
 let
   if-outside-home = "! ${pkgs.iw}/bin/iw dev wlp1s0 info | grep -q Dziupla";
 
+  matchBlocks = lib: {
+    archive = {
+      hostname = "192.168.1.200";
+      port = 33002;
+      user = "pwy";
+    };
+
+    archive--ext = lib.hm.dag.entryAfter [ "archive" ] {
+      match = ''OriginalHost archive Exec "${if-outside-home}"'';
+      hostname = "10.24.1.2";
+      proxyJump = "gateway";
+      forwardAgent = true;
+    };
+
+    gateway = {
+      hostname = "142.132.178.21";
+      port = 33000;
+      user = "pwy";
+    };
+
+    glacier = {
+      hostname = "u393702.your-storagebox.de";
+      port = 23;
+      user = "u393702";
+    };
+
+    sienna = {
+      hostname = "192.168.1.220";
+      user = "pi";
+    };
+
+    ubu = {
+      hostname = "192.168.122.227";
+      user = "pwy";
+    };
+
+    warp = {
+      hostname = "192.168.1.200";
+      port = 33000;
+      user = "pwy";
+    };
+
+    warp--ext = lib.hm.dag.entryAfter [ "warp" ] {
+      match = ''OriginalHost warp Exec "${if-outside-home}"'';
+      hostname = "10.24.1.2";
+      proxyJump = "gateway";
+      forwardAgent = true;
+    };
+
+    warp-ubu = {
+      hostname = "192.168.122.92";
+      user = "pwy";
+      proxyJump = "warp";
+    };
+  };
+
 in
 {
+  home-manager.users.root =
+    { lib, ... }:
+    {
+      programs = {
+        ssh = {
+          enable = true;
+
+          matchBlocks = {
+            inherit (matchBlocks lib)
+              archive
+              archive--ext
+              gateway
+              warp
+              warp--ext
+              ;
+          };
+        };
+      };
+    };
+
   home-manager.users.pwy =
     { lib, ... }:
     {
       programs = {
         ssh = {
-          matchBlocks = {
-            archive = {
-              hostname = "192.168.1.200";
-              port = 33002;
-              user = "pwy";
-            };
-
-            archive--ext = lib.hm.dag.entryAfter [ "archive" ] {
-              match = ''OriginalHost archive Exec "${if-outside-home}"'';
-              hostname = "10.24.1.2";
-              proxyJump = "gateway";
-              forwardAgent = true;
-            };
-
-            gateway = {
-              hostname = "142.132.178.21";
-              port = 33000;
-              user = "pwy";
-            };
-
-            glacier = {
-              hostname = "u393702.your-storagebox.de";
-              port = 23;
-              user = "u393702";
-            };
-
-            sienna = {
-              hostname = "192.168.1.220";
-              user = "pi";
-            };
-
-            ubu = {
-              hostname = "192.168.122.227";
-              user = "pwy";
-            };
-
-            warp = {
-              hostname = "192.168.1.200";
-              port = 33000;
-              user = "pwy";
-            };
-
-            warp--ext = lib.hm.dag.entryAfter [ "warp" ] {
-              match = ''OriginalHost warp Exec "${if-outside-home}"'';
-              hostname = "10.24.1.2";
-              proxyJump = "gateway";
-              forwardAgent = true;
-            };
-
-            warp-ubu = {
-              hostname = "192.168.122.92";
-              user = "pwy";
-              proxyJump = "warp";
-            };
-          };
+          matchBlocks = matchBlocks lib;
         };
       };
     };
--- nodes/fw/user.nix
+++ nodes/fw/user.nix
@@ -12,27 +12,6 @@
           username = "root";
           stateVersion = "24.05";
         };
-
-        programs = {
-          ssh = {
-            enable = true;
-
-            matchBlocks = {
-              archive = {
-                port = 33002;
-                user = "pwy";
-                hostname = "10.24.1.2";
-                proxyJump = "gateway";
-              };
-
-              gateway = {
-                hostname = "142.132.178.21";
-                port = 33000;
-                user = "pwy";
-              };
-            };
-          };
-        };
       };
 
       pwy = {
