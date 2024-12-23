55b3ffddc449f68158b93248dd3b14161f5f8e17
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-12-23 14:59:37 +0100

    fw: Setup wg-fort

--- nodes/fw/net.nix
+++ nodes/fw/net.nix
@@ -7,6 +7,26 @@
     networkmanager = {
       dns = "systemd-resolved";
     };
+
+    wireguard = {
+      interfaces = {
+        wg-fort = {
+          ips = [ "10.24.1.10/24" ];
+          listenPort = 51820;
+          privateKeyFile = "/var/secrets/wg-fort/private.key";
+
+          peers = [
+            # gateway
+            {
+              publicKey = "GwhWP0DClVw9fY7PJidPuZfOzBhxhcnjTnO+8i1Z50w=";
+              allowedIPs = [ "10.24.1.0/24" ];
+              endpoint = "142.132.178.21:51820";
+              persistentKeepalive = 10;
+            }
+          ];
+        };
+      };
+    };
   };
 
   services = {
