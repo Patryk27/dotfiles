4218df6692b45575d7040e1a7d30e01d5ca3a1db
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-03-31 13:52:03 +0200

    fmt: Drop unused flake input

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
       url = "github:ryantm/agenix";
     };
 
-    emacs-mac = {
-      url = "https://bitbucket.org/mituharu/emacs-mac/get/7cc5e67629363d9e98f65e4e652f83bb4e0ee674.zip";
-      flake = false;
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager";
 
